5c05212b7225a94145846969e98f1d6669d69a99
HAMEER KU <ameerku83@gmail.com>
2024-08-05 12:51:45 +0530

    2nd

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://ameerku-cred-app.onrender.com'
 });
 
 export const apiProduct = {
